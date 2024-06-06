aa14f47ac99b367d53202a882195cac6215d7fb3
mesyamaureen <mesyamaureen@gmail.com>
2024-06-06 16:46:02 +0200

    spring boot app erstellt

---
+++ src/test/java/masys/nachhaltigkeits_dashboard/NachhaltigkeitsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package masys.nachhaltigkeits_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NachhaltigkeitsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
