25a9f647acb6d688662242005ac1823f4562b240
JLkn <107927104+JLkn@users.noreply.github.com>
2024-06-21 10:45:48 +0200

    Added Dashboard Entity

--- src/main/java/masys/nachhaltigkeits_dashboard/entities/Dashboard.java
+++ src/main/java/masys/nachhaltigkeits_dashboard/entities/Dashboard.java
@@ -1,8 +1,53 @@
 package masys.nachhaltigkeits_dashboard.entities;
 
+import jakarta.persistence.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public class Dashboard {
+import java.util.List;
 
+@Entity
+public class Dashboard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private Benutzer benutzer;
+
+    @ManyToMany
+    private List<Kpi> kpis;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Benutzer getBenutzer() {
+        return benutzer;
+    }
+
+    public void setBenutzer(Benutzer benutzer) {
+        this.benutzer = benutzer;
+    }
+
+    public List<Kpi> getKpis() {
+        return kpis;
+    }
+
+    public void setKpis(List<Kpi> kpis) {
+        this.kpis = kpis;
+    }
 }
