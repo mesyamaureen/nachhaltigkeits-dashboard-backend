51c39333b7dd88e164527bd60569fd11975f20f8
JLkn <107927104+JLkn@users.noreply.github.com>
2024-06-21 11:00:28 +0200

    Added BenutzerController RestController

--- src/main/java/masys/nachhaltigkeits_dashboard/controllers/BenutzerController.java
+++ src/main/java/masys/nachhaltigkeits_dashboard/controllers/BenutzerController.java
@@ -1,7 +1,14 @@
 package masys.nachhaltigkeits_dashboard.controllers;
 
+import masys.nachhaltigkeits_dashboard.repositories.BenutzerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/benutzer")
 public class BenutzerController {
+    @Autowired
+    private BenutzerRepository benutzerRepository;
+
 }
