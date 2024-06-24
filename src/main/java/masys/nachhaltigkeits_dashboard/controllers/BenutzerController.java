e98ffd9bf5bc6a3338108f7bd65d4912efc389fe
JanLueken <jan.lueken98@gmx.de>
2024-06-24 20:00:00 +0200

    BenutzerKpi Entität hinzugefügt

--- src/main/java/masys/nachhaltigkeits_dashboard/controllers/BenutzerController.java
+++ src/main/java/masys/nachhaltigkeits_dashboard/controllers/BenutzerController.java
@@ -1,12 +1,16 @@
 package masys.nachhaltigkeits_dashboard.controllers;
 
 import masys.nachhaltigkeits_dashboard.entities.Benutzer;
+import masys.nachhaltigkeits_dashboard.entities.BenutzerKpi;
+import masys.nachhaltigkeits_dashboard.entities.Kpi;
+import masys.nachhaltigkeits_dashboard.repositories.BenutzerKpiRepository;
 import masys.nachhaltigkeits_dashboard.repositories.BenutzerRepository;
+import masys.nachhaltigkeits_dashboard.repositories.KpiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/benutzer")
@@ -14,9 +18,42 @@ public class BenutzerController {
     @Autowired
     private BenutzerRepository benutzerRepository;
 
+    @Autowired
+    private KpiRepository kpiRepository;
+
+    @Autowired
+    private BenutzerKpiRepository benutzerKpiRepository;
+
     @PostMapping("signup")
     public void signup(@RequestBody Benutzer benutzer) {
-
+        benutzerRepository.save(benutzer);
     }
 
+    @PostMapping("{id}/kpis")
+    public void addKpiToBenutzer(@PathVariable Long id, @RequestBody Long kpiId) {
+        Benutzer benutzer = benutzerRepository.findById(id).orElseThrow();
+        Kpi kpi = kpiRepository.findById(kpiId).orElseThrow();
+        BenutzerKpi benutzerKpi = new BenutzerKpi();
+        benutzerKpi.setBenutzer(benutzer);
+        benutzerKpi.setKpi(kpi);
+        benutzerKpiRepository.save(benutzerKpi);
+    }
+
+    @DeleteMapping("{id}/kpis/{kpiId}")
+    public void removeKpiFromBenutzer(@PathVariable Long id, @PathVariable Long kpiId) {
+        Benutzer benutzer = benutzerRepository.findById(id).orElseThrow();
+        BenutzerKpi benutzerKpi = benutzerKpiRepository.findByBenutzerId(id)
+                .stream()
+                .filter(bk -> bk.getKpi().getId().equals(kpiId))
+                .findFirst()
+                .orElseThrow();
+        benutzerKpiRepository.delete(benutzerKpi);
+    }
+
+    @GetMapping("{id}/kpis")
+    public List<Kpi> getBenutzerKpis(@PathVariable Long id) {
+        Benutzer benutzer = benutzerRepository.findById(id).orElseThrow();
+        List<BenutzerKpi> benutzerKpis = benutzerKpiRepository.findByBenutzerId(benutzer.getId());
+        return benutzerKpis.stream().map(BenutzerKpi::getKpi).toList();
+    }
 }
