09764c95bcb2674494589e1ae318c2ce9576b1e9
mesyamaureen <mesyamaureen@gmail.com>
2024-07-11 23:47:25 +0200

    endpoint geändert

--- src/main/java/masys/nachhaltigkeits_dashboard/websettings/WebConfig.java
+++ src/main/java/masys/nachhaltigkeits_dashboard/websettings/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://nachhaltigkeits-dashboard-frontend.onrender.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("https://nachhaltigkeits-dashboard-frontend.onrender.com")
+                .allowedMethods("*");
+                // .allowedHeaders("*")
+                // .allowCredentials(true);
     }
 }
